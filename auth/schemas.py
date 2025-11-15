e25f617414266e95d75644bf7852ca0410d69dc2
Adamandk <adamandk@gmail.com>
2025-11-16 01:47:45 +0500

    Update schemas.py

--- auth/schemas.py
+++ auth/schemas.py
@@ -9,6 +9,9 @@ class LoginRequest(BaseModel):
 class LoginResponse(BaseModel):
     access_token: str
     token_type: str = "bearer"
+
     party_id: str
     identity_id: str
     role_code: str
+    my_company_id: str | None = None
+    display_name: str
